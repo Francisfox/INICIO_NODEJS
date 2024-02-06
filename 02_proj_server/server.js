eec7cff03c390a28aca0112f6ba50f4f50789a70
Francisfox <francismarfox@hotmail.com>
2024-02-06 19:13:48 +0000

    -

--- 02_proj_server/server.js
+++ 02_proj_server/server.js
@@ -1,8 +1,13 @@
 //npm init -y
 //npm isntall express --save
 
-const app = require('express')();
-app.listen(3001, () => console.log(`servidor rodandando`));
+const { Socket } = require('dgram');
+
+const app = require('express')()
+const server = require('http').createServer(app)
+//const io = require('Socket.io')(server,{cors: {origin:'https://3000-francisfox-inicionodejs-cbzu6jtzyy9.ws-us108.gitpod.io/'}})
+const PORT = 3001
+app.listen(PORT, () => console.log(`servidor rodandando`));
 app.get('/',(req, res) => {
     res.send('Javascript and node programacao' );
     //res.send({javascript:10,node:10}); //pode usar um objeto que ele aparece em json na tela
