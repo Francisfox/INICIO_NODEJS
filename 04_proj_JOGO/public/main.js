d393425686c4f38caf69566aa347d15881c2e33c
Francisfox <francismarfox@hotmail.com>
2024-02-09 04:07:08 +0000

    -

--- 04_proj_JOGO/public/main.js
+++ 04_proj_JOGO/public/main.js
@@ -6,6 +6,35 @@ window.addEventListener('load',function(){
     canvas.width = 500;
     canvas.height = 500;
 
+    class InputHandler {
+        constructor(){
+            this.keys = [];
+            window.addEventListener('keydown',e => {
+                if((e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight' ||
+                    e.key === 'Enter'
+                    ) && this.keys.indexOf(e.key) === -1){
+                    this.keys.push(e.key);
+                }
+                console.log(e.key , this.keys);
+            });
+            window.addEventListener('keyup',e => {
+                console.log(e.key,this.keys);
+                if( e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight' ||
+                    e.key === 'Enter'){
+                    this.keys.splice(this.keys.indexOf(e.key), 1)
+                }
+                console.log(e.key,this.keys);
+            });            
+
+
+        }
+    }
     class Player {
         constructor(game){
             this.game = game;
@@ -13,13 +42,31 @@ window.addEventListener('load',function(){
             this.height = 91.3;
             this.x = 0;
             this.y = this.game.height - this.height;
+            this.vy = 0;
+            this.weight = 1;
+            this.image = document.getElementById('player')
+            this.speed = 0;
+            this.maxSpeed = 5;
         }
-        update(){
-
+        update(input){
+            //horizontal moviment
+            this.x += this.speed;
+            if(input.includes('ArrowRight')) this.speed = this.maxSpeed;
+            else if(input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+            else this.speed = 0;
+            if(this.x < 0 ) this.x = 0;
+            if(this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+            //vertical moviment
+            if(input.includes('ArrowUp') && this.onGround()) this.vy -= 20;
+            this.y += this.vy;
+            if(!this.onGround()) this.vy += this.weight;
+            else this.vy = 0;
         }
         draw(context){
-            context.fillStyle = 'red'
-            context.fillRect(this.x,this.y, this.width,this.height);
+            context.drawImage(this.image,0,0,this.width,this.height,this.x,this.y,this.width,this.height );
+        }
+        onGround(){
+            return this.y >=this.game.height - this.height;
         }
     }
 
@@ -28,9 +75,10 @@ window.addEventListener('load',function(){
             this.width = width;
             this.height = height;
             this.player = new Player(this);
+            this.imput = new InputHandler();
         }
         update(){
-
+            this.player.update(this.imput.keys);
         }
         draw(context){
             this.player.draw(context);
@@ -41,6 +89,8 @@ window.addEventListener('load',function(){
     console.log(game);
 
     function animate(){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        game.update();
         game.draw(ctx);
         requestAnimationFrame(animate);
     }
